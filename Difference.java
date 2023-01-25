bdbd2d235d1b57ffa1313279ca09889a625c8538
PrithaBhattacharya3010 <pritha3010b@outlook.com>
2023-01-25 07:13:36 +0530

    Difference.java added

---
+++ Difference.java
@@ -0,0 +1,9 @@
+public class Difference {
+    public static void main(String[] args) {
+        int a = 507;
+        int b=7;
+        int c = a-b;
+        System.out.println("The Diff is "+c);
+    
+    }
+}

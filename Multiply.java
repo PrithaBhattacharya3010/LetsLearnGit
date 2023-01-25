3a85c7a2deac718f37b72985c1801a93ab0f321f
PrithaBhattacharya3010 <pritha3010b@outlook.com>
2023-01-25 07:31:30 +0530

    Added Multiplication

---
+++ Multiply.java
@@ -0,0 +1,10 @@
+public class Multiply {
+    public static void main(String[] args) {
+        int a = 507;
+        int b=7;
+        int c = a*b;
+        System.out.println("hi");
+        System.out.println("The Multiplication  is "+c);
+    
+    }
+}

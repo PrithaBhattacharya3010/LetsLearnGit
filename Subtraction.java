2ab2afc06b8afa0093f873495632b7b88a72b43c
PrithaBhattacharya3010 <pritha3010b@outlook.com>
2023-02-02 14:15:07 +0530

    Subtraction File Added

---
+++ Subtraction.java
@@ -0,0 +1,15 @@
+public class Subtraction {
+    public static void main(String[] args) {
+        int a = 507;
+        int c=0;
+        int b=7;
+        if(a>b){
+        c = a/b;}
+        else{
+            c=b/a;
+        }
+        System.out.println("hi");
+        System.out.println("The Multiplication  is "+c);
+    
+    }
+}

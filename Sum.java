0368c56098f82c1faffbfd958659d12ec4175d7a
PrithaBhattacharya3010 <pritha3010b@outlook.com>
2023-02-02 15:17:07 +0530

    Sum.java updated

--- Sum.java
+++ Sum.java
@@ -2,7 +2,8 @@ public class Sum {
     public static void main(String[] args) {
         int a = 5;
         int b=7;
-        int c = a+b;
+        int k=9;
+        int c = a+b+k;
         System.out.println("The Sum is "+c);
     }
 }
